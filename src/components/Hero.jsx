6c8e64c9b6a830001ca92cb732015f1524b5a23e
Ayush <ayushlucky2003@gmail.com>
2025-08-24 11:37:02 +0530

    Implemented Video Animation in Hero Section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,14 @@
 import { useGSAP } from '@gsap/react'
 import gsap from 'gsap'
 import { SplitText } from 'gsap/all'
-import React from 'react'
+import React, { useRef } from 'react'
+import { useMediaQuery } from 'react-responsive'
 
 const Hero = () => {
+    const videoRef =  useRef();
+    const isMobile = useMediaQuery({maxWidth: 767});
+
+
     useGSAP(() => {
         const heroSplit = new SplitText('.title', {type: 'chars, words'});
         const paragraphSplit = new SplitText('.subtitle', {type: 'lines'});
@@ -35,7 +40,26 @@ const Hero = () => {
         }).to('.right-leaf', {y:200}, 0)
         .to('.left-leaf', {y:-200}, 0)
 
-    }, [])
+        const startValue = isMobile ? "top 50%" : "center 60%";
+	    const endValue = isMobile ? "120% top" : "bottom top";
+	
+        let tl = gsap.timeline({
+        scrollTrigger: {
+            trigger: "video",
+            start: startValue,
+            end: endValue,
+            scrub: true,
+            pin: true,
+        },
+        });
+	
+        videoRef.current.onloadedmetadata = () => {
+        tl.to(videoRef.current, {
+            currentTime: videoRef.current.duration,
+        });
+        };
+    }, []);
+
   return (
     <>
         <section id='hero' className='noisy'>
@@ -61,12 +85,23 @@ const Hero = () => {
                     <p className='subtitle'>
                         Every cocktail on our menu is a blend of premium ingredients, creative flair, and timeless recipes -designed to delight your senses
                     </p>
-                    <a href="#cocktails">View CockTails</a>
+                    <br />
+                    <a href="#cocktails" className='font-bold'>View CockTails</a>
 
 
                 </div>
               </div>
         </section>
+        <div className='video absolute inset-0'>
+            <video
+                ref={videoRef}
+                src='/videos/output.mp4'
+                muted
+                playsInline
+                preload='auto'     
+            />
+
+        </div>
     </>
   )
 }
